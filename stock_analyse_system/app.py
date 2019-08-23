29a61fa4871b4b44e105a7f3bf18cfe14ba93256
郑净云 <343070065@qq.com>
2019-08-23 18:16:31 +0800

    引入bootstap  设计首页模板

--- stock_analyse_system/app.py
+++ stock_analyse_system/app.py
@@ -3,6 +3,8 @@ from auth import login
 from flask_script import Manager
 app = Flask(__name__)
 app.register_blueprint(login.blueprint)
+app.jinja_env.auto_reload = True
+app.run(debug=True)
 manager = Manager(app)
 
 if __name__ == '__main__':
