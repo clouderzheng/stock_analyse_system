bd3101f5a583b8a5d5bb7f334e40f6ea616d6852
郑净云 <zhengjingyun@unionbigdata.com>
2019-11-04 15:04:42 +0800

    提交config

---
+++ stock_analyse_system/python/mysql/myql_config.py
@@ -0,0 +1,10 @@
+DBHOST = "122.51.81.57"
+DBPORT = 3306
+DBUSER = "root"
+# DBPWD = "123456789"
+DBPWD = "111"
+DBNAME = "trade_system"
+DBCHAR = "utf8mb4"
+
+jq_account = "13350865639"
+jq_password = "yun13350865639"
