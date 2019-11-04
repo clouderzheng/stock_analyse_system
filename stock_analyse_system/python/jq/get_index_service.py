9cee59b26832919aa1013b70d036478b0f4d807b
night1994 <343070065@qq.com>
2019-11-04 21:57:15 +0800

    去除首页使用jqdatasdk

--- stock_analyse_system/python/jq/get_index_service.py
+++ stock_analyse_system/python/jq/get_index_service.py
@@ -1,46 +1,46 @@
-from python.jq.get_international_indice import InternationalIndice
-import numpy as np
-import math
-def get_international_index(result,code ,code_name):
-
-    internationalIndice = InternationalIndice()
-    # 获取纳克达斯指数
-    data = internationalIndice.get_International_Index(code)
-
-    # 按照时间先后排序 升序
-    data = data.sort_values(by="day", axis=0, ascending=True)
-    deal_with_data(result,data,code_name,"day")
-
-def get_internal_index_service(result,code,code_name):
-    internationalIndice = InternationalIndice()
-    data = internationalIndice.get_internal_Index(code)
-    deal_with_data(result,data,code_name,"index")
-
-def deal_with_data(result,data , suff,sort_column):
-
-    times = []
-    # 获取时间作为x轴
-    if("index" == sort_column):
-        date_ = data.index.date
-        for _date in date_:
-            times.append(str(_date))
-    else:
-        for _date in np.array(data[sort_column]).tolist():
-            times.append(str(_date))
-
-
-    # 开盘价 收盘价 最高价 最低价最为 y轴
-    view_data = np.array(data[['open', 'close', 'low', 'high']]).tolist()
-    # 指数名称作为提示指标
-    code_name =suff
-    result["times_" + suff] =  times
-    result["view_data_" + suff] =  view_data
-    result["code_name_" + suff] =  code_name
-    volumes = np.array(data['volume']).tolist()
-    result["volume_" + suff] = volumes
-    result["volume_yaxis_" + suff] =  math.ceil(max(volumes) * 3)
-# import pandas as pd
-# internationalIndice = InternationalIndice()
-# data = internationalIndice.get_SSE_50_Index(100)
-# # print(type(data.index[0]))
-# date = data.index.date
+# from python.jq.get_international_indice import InternationalIndice
+# import numpy as np
+# import math
+# def get_international_index(result,code ,code_name):
+#
+#     internationalIndice = InternationalIndice()
+#     # 获取纳克达斯指数
+#     data = internationalIndice.get_International_Index(code)
+#
+#     # 按照时间先后排序 升序
+#     data = data.sort_values(by="day", axis=0, ascending=True)
+#     deal_with_data(result,data,code_name,"day")
+#
+# def get_internal_index_service(result,code,code_name):
+#     internationalIndice = InternationalIndice()
+#     data = internationalIndice.get_internal_Index(code)
+#     deal_with_data(result,data,code_name,"index")
+#
+# def deal_with_data(result,data , suff,sort_column):
+#
+#     times = []
+#     # 获取时间作为x轴
+#     if("index" == sort_column):
+#         date_ = data.index.date
+#         for _date in date_:
+#             times.append(str(_date))
+#     else:
+#         for _date in np.array(data[sort_column]).tolist():
+#             times.append(str(_date))
+#
+#
+#     # 开盘价 收盘价 最高价 最低价最为 y轴
+#     view_data = np.array(data[['open', 'close', 'low', 'high']]).tolist()
+#     # 指数名称作为提示指标
+#     code_name =suff
+#     result["times_" + suff] =  times
+#     result["view_data_" + suff] =  view_data
+#     result["code_name_" + suff] =  code_name
+#     volumes = np.array(data['volume']).tolist()
+#     result["volume_" + suff] = volumes
+#     result["volume_yaxis_" + suff] =  math.ceil(max(volumes) * 3)
+# # import pandas as pd
+# # internationalIndice = InternationalIndice()
+# # data = internationalIndice.get_SSE_50_Index(100)
+# # # print(type(data.index[0]))
+# # date = data.index.date
--- stock_analyse_system/python/system/home_index.py
+++ stock_analyse_system/python/system/home_index.py
@@ -1,25 +1,25 @@
-from python.jq import get_index_service
+# from python.jq import get_index_service
 from flask import Blueprint
 from python.system import system_code
-from python.jq import jq_config
+# from python.jq import jq_config
 blueprint = Blueprint('home', __name__, url_prefix='/home')
 
 @blueprint.route('/index', methods=['POST'])
 def get_home_index():
     result = {"code":system_code.success_code}
     """获取纳斯达克指数信息"""
-    get_index_service.get_international_index(result,jq_config.jq_stock_ixic_code,jq_config.jq_stock_ixic_code_name)
-    """获取道琼斯指数信息"""
-    get_index_service.get_international_index(result,jq_config.jq_stock_dji_code,jq_config.jq_stock_dji_code_name)
-    """获取标普500指数信息"""
-    get_index_service.get_international_index(result,jq_config.jq_stock_inx_code,jq_config.jq_stock_inx_code_name)
-    """获取纳斯达克指数信息"""
-    get_index_service.get_international_index(result,jq_config.jq_stock_ixic_code,jq_config.jq_stock_ixic_code_name)
-    """获取上证指数"""
-    get_index_service.get_internal_index_service(result,jq_config.jq_stock_sse_code,jq_config.jq_stock_sse_code_name)
-    """获取深证成指"""
-    get_index_service.get_internal_index_service(result,jq_config.jq_stock_szcz_code,jq_config.jq_stock_szcz_code_name)
-    """获取创业板指"""
-    get_index_service.get_internal_index_service(result,jq_config.jq_stock_cybz_code,jq_config.jq_stock_cybz_code_name)
+    # get_index_service.get_international_index(result,jq_config.jq_stock_ixic_code,jq_config.jq_stock_ixic_code_name)
+    # """获取道琼斯指数信息"""
+    # get_index_service.get_international_index(result,jq_config.jq_stock_dji_code,jq_config.jq_stock_dji_code_name)
+    # """获取标普500指数信息"""
+    # get_index_service.get_international_index(result,jq_config.jq_stock_inx_code,jq_config.jq_stock_inx_code_name)
+    # """获取纳斯达克指数信息"""
+    # get_index_service.get_international_index(result,jq_config.jq_stock_ixic_code,jq_config.jq_stock_ixic_code_name)
+    # """获取上证指数"""
+    # get_index_service.get_internal_index_service(result,jq_config.jq_stock_sse_code,jq_config.jq_stock_sse_code_name)
+    # """获取深证成指"""
+    # get_index_service.get_internal_index_service(result,jq_config.jq_stock_szcz_code,jq_config.jq_stock_szcz_code_name)
+    # """获取创业板指"""
+    # get_index_service.get_internal_index_service(result,jq_config.jq_stock_cybz_code,jq_config.jq_stock_cybz_code_name)
     return result
 
