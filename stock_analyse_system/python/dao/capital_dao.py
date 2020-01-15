ec99720129de144b668738aaae53b4a3e7fa5a55
night1994 <343070065@qq.com>
2020-01-15 23:09:06 +0800

    添加龙虎榜数据

--- stock_analyse_system/python/dao/capital_dao.py
+++ stock_analyse_system/python/dao/capital_dao.py
@@ -17,3 +17,9 @@ class capital_dao():
     def query_sale_department_capital(self):
         sql = "SELECT stock_name,stock_code,create_date,SUM(capital_in_flow) as in_flow,SUM(capital_out_flow) as outflow, sum(capital_net_flow) as a1 from trade_sale_department_capital_flow  GROUP BY stock_code ORDER BY a1 desc"
         return self.mysqlService.selectMany(sql)
+
+    """记录龙虎榜数据"""
+    def add_tiger_list(self,param):
+        sql = "insert into trade_stock_tiger_list (`stock_code`,`stock_name`,`explain`,close_price,rate,tiger_in_flow,tiger_net_flow,tiger_out_flow,tiger_total_flow,market_total_flow,tiger_total_in_market_rate,turnover_rate,circulate_money,list_reason,create_date) \
+            value (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        self.mysqlService.insert(sql,param)
--- stock_analyse_system/python/service/money_flow_service.py
+++ stock_analyse_system/python/service/money_flow_service.py
@@ -3,6 +3,11 @@ from python.service import crawl_html_url
 from python.dao import capital_dao
 import requests
 import json
+import io
+
+import sys
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
 """资金流向service"""
 class money_flow:
 
@@ -12,12 +17,12 @@ class money_flow:
 
     """获取每日活跃营业部 买卖股票信息"""
     def get_active_sale_department(self):
-
         current_day = date_time_util.get_date(0)
         count = "500"
         # 查询当日活跃营业部
         url = crawl_html_url.east_money_active_sale_department.format(count,current_day,current_day)
         data = requests.get(url)
+        data.encoding = "GBK"
         sales_department = json.loads(data.text[15:])["data"]
         # 遍历每个营业部买卖股票
         for sale_department in sales_department:
@@ -28,7 +33,10 @@ class money_flow:
             stock_arry = json.loads(stocks)
 
             #查询改营业部下 每个股票的资金流入
-            single_stocks_capital = json.loads(requests.get(crawl_html_url.east_money_active_sale_department_trade_stock_money.format(len(stock_arry),department_code)).text[13:])["data"]
+            stock = requests.get(
+                crawl_html_url.east_money_active_sale_department_trade_stock_money.format(len(stock_arry),department_code))
+            stock.encoding = "GBK"
+            single_stocks_capital = json.loads(stock.text[13:])["data"]
             for stock_capital in single_stocks_capital:
                 param = []
                 param.append(stock_capital["SalesName"])
@@ -47,6 +55,32 @@ class money_flow:
         data = self.capital_dao.query_sale_department_capital()
         return data
 
-a = money_flow()
-a.get_active_sale_department()
 
+    """获取龙虎榜数据"""
+    def get_tiger_list(self):
+        today = date_time_util.get_date(0)
+        data = requests.get(crawl_html_url.east_monet_tiger_list.format(today))
+        data.encoding = "gbk"
+        stock_names = json.loads(data.text[15:])["data"]
+        for stock in stock_names:
+            param = []
+            param.append(stock["SCode"])
+            param.append(stock["SName"])
+            param.append(stock["JD"])
+            param.append(stock["ClosePrice"])
+            param.append(stock["Chgradio"])
+            param.append(stock["Bmoney"])
+            param.append(0 if stock["JmMoney"] == '' else stock["JmMoney"])
+            param.append(0 if stock["Smoney"] == '' else stock["Smoney"])
+            param.append(stock["ZeMoney"])
+            param.append(stock["Turnover"])
+            param.append(stock["JmRate"])
+            param.append(stock["Dchratio"])
+            param.append(stock["Ltsz"])
+            param.append(stock["Ctypedes"])
+            param.append(date_time_util.get_date(0))
+            self.capital_dao.add_tiger_list(param)
+
+a = money_flow()
+# a.get_active_sale_department()
+a.get_tiger_list()
